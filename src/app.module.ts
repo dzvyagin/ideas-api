8b3b410fb1d43d2b089ce643beb8ac6795e28b24
dzvyagin <dzvyagin@team.amocrm.com>
2022-06-08 01:41:56 +0300

    add error filter and interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IdeaModule } from './idea/idea.module';
+import { HttpErrorFilter } from './shared/http-error.filter';
+import { LoggingInterceptor } from './shared/logging.interceptor';
+
 import ormconfig from './ormconfig';
 
 @Module({
   imports: [TypeOrmModule.forRoot(ormconfig), IdeaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/shared/http-error.filter.ts
@@ -0,0 +1,34 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpErrorFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    const errorResponse = {
+      code: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      method: request.method,
+      message: exception.message || null,
+    };
+
+    Logger.error(
+      `${request.method} ${request.url}`,
+      JSON.stringify(errorResponse),
+      'ExceptionFilter',
+    );
+
+    response.status(status).json(errorResponse);
+  }
+}
