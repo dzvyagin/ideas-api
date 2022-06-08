ced5a76d33922f8b2b58758057de4c3d0261bd1b
dzvyagin <dzvyagin@team.amocrm.com>
2022-06-09 01:52:34 +0300

    add validation pipe

--- src/idea/idea.controller.ts
+++ src/idea/idea.controller.ts
@@ -3,23 +3,31 @@ import {
   Controller,
   Delete,
   Get,
+  Logger,
   Param,
   Post,
   Put,
+  UsePipes,
 } from '@nestjs/common';
 import { IdeaDTO } from './idea.dto';
 import { IdeaService } from './idea.service';
+import { ValidationPipe } from '../shared/validation.pipe';
 
 @Controller('idea')
 export class IdeaController {
+  private logger = new Logger('IdeaController');
+
   constructor(private ideaService: IdeaService) {}
+
   @Get()
   showAllIdeas() {
     return this.ideaService.showAll();
   }
 
   @Post()
+  @UsePipes(new ValidationPipe())
   createIdea(@Body() data: IdeaDTO) {
+    this.logger.log(JSON.stringify(data));
     return this.ideaService.create(data);
   }
 
@@ -29,7 +37,9 @@ export class IdeaController {
   }
 
   @Put(':id')
+  @UsePipes(new ValidationPipe())
   updateIdea(@Param('id') id: string, @Body() data: Partial<IdeaDTO>) {
+    this.logger.log(JSON.stringify(data));
     return this.ideaService.update(id, data);
   }
 
