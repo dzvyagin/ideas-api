0b8cf7cf6ed97b40813707fb6d1b974200a6cac8
dzvyagin <dzvyagin@team.amocrm.com>
2022-06-13 00:29:12 +0300

    update user api and .env file

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,22 +1,25 @@
-// import { Body, Controller, Get, Post } from '@nestjs/common';
-// import { UserService } from './user.service';
+import { Body, Controller, Get, Post, UsePipes } from '@nestjs/common';
+import { UserDTO } from './user.dto';
+import { UserService } from './user.service';
+import { ValidationPipe } from './../shared/validation.pipe';
 
-// @Controller()
-// export class UserController {
-//   constructor(private userService: UserService) {}
+@Controller()
+export class UserController {
+  constructor(private userService: UserService) {}
 
-//   @Get('api/users')
-//   showAllUsers() {
-//     return this.userService.showAll();
-//   }
+  @Get('api/users')
+  showAllUsers() {
+    return this.userService.showAll();
+  }
 
-//   @Post('login')
-//   login(@Body() data) {
-//     return this.userService.login(data);
-//   }
+  @Post('login')
+  @UsePipes(new ValidationPipe())
+  login(@Body() data: UserDTO) {
+    return this.userService.login(data);
+  }
 
-//   @Post('register')
-//   register(@Body() data) {
-//     return this.userService.register(data);
-//   }
-// }
+  @Post('register')
+  register(@Body() data: UserDTO) {
+    return this.userService.register(data);
+  }
+}
